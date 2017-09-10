c6e9e3e428e8289f4c5fee9cb99650215c00aa5f
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-09-10 01:52:45 -0500

    click event added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+ // prevented = false;
+  homeImageList = [
+    {image: "assets/images/natrue/1.jpg",title: "Image 1",link: "videos/video-1"},
+    {image: "assets/images/natrue/2.jpg",title: "Image 1",link: "videos/video-2"},
+    {image: "assets/images/natrue/3.jpg",title: "Image 1",link: "videos/video-3"}
 
-  constructor() { }
+  ]
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
+  preventNormal(event:MouseEvent, image:any){
+    if(!image.prevented){
+      event.preventDefault()
+      //console.log(image.getAttribute("href"))
+     // image.setAttribute("href","/videos")
+      //image.link = "/videos"
+     // image.prevented = true;
+      this.router.navigate(['./videos'])
 
+    }
+    //console.log(event)
+  }
 }
