558f0971d6782798942430721a11de133657beec
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-09-09 21:50:41 -0500

    ng-x bootstrap added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+//third party imports
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 import { AppComponent } from './app.component';
 import { VideoListComponent } from './video-list/video-list.component';
 import { VideoDetailsComponent } from './video-details/video-details.component';
@@ -15,6 +19,7 @@ import { SafePipe } from './utility/safe.pipe';
     SafePipe
   ],
   imports: [
+    BsDropdownModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
